b89163f38e79e6ae455dc2dfb27ebac2f6ccee1e
iijima <princecharlesalexander@yahoo.co.jp>
2019-12-01 16:11:36 +0900

    整形

--- Startup.cs
+++ Startup.cs
@@ -44,40 +44,41 @@ namespace src
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSignalR().AddAzureSignalR();
 
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie()
-    .AddGitHub(options =>
-    {
-        options.ClientId = Configuration[GitHubClientId];
-        options.ClientSecret = Configuration[GitHubClientSecret];
-        options.Scope.Add("user:email");
-        options.Events = new OAuthEvents
-        {
-            OnCreatingTicket = GetUserCompanyInfoAsync
-        };
-    });
+            services
+            .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie()
+            .AddGitHub(options =>
+            {
+                options.ClientId = Configuration[GitHubClientId];
+                options.ClientSecret = Configuration[GitHubClientSecret];
+                options.Scope.Add("user:email");
+                options.Events = new OAuthEvents
+                {
+                    OnCreatingTicket = GetUserCompanyInfoAsync
+                };
+            });
         }
 
         private static async Task GetUserCompanyInfoAsync(OAuthCreatingTicketContext context)
-{
-    var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
-    request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-
-    var response = await context.Backchannel.SendAsync(request,
-        HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
-
-    var user = JObject.Parse(await response.Content.ReadAsStringAsync());
-    if (user.ContainsKey("company"))
-    {
-        var company = user["company"].ToString();
-        var companyIdentity = new ClaimsIdentity(new[]
         {
-            new Claim("Company", company)
-        });
-        context.Principal.AddIdentity(companyIdentity);
-    }
-}
+            var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+            var response = await context.Backchannel.SendAsync(request,
+                HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+
+            var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+            if (user.ContainsKey("company"))
+            {
+                var company = user["company"].ToString();
+                var companyIdentity = new ClaimsIdentity(new[]
+                {
+                    new Claim("Company", company)
+                });
+                context.Principal.AddIdentity(companyIdentity);
+            }
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
