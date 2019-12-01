d30759fd98d2622565d030e71c7a36752cc5e6b0
iijima <princecharlesalexander@yahoo.co.jp>
2019-12-01 18:03:17 +0900

    コメント削除

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-
-function getCookie(key) {
+﻿function getCookie(key) {
     var cookies = document.cookie.split(';').map(c => c.trim());
     for (var i = 0; i < cookies.length; i++) {
         if (cookies[i].startsWith(key + '=')) return unescape(cookies[i].slice(key.length + 1));
